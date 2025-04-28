2a05338b4758b25513102cbc82a647562f55abe1
AndrewFesenko <andrewfesenko10@gmail.com>
2025-04-28 17:44:00 -0400

    implement interview generation page ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "EchoHire",
   description: "An AI-Powered Platform for Preparing for Mock Interviews",
 };
 
