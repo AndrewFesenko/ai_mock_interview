a4909f1f1e371df1974db735f701a6bc61fb6bd2
AndrewFesenko <andrewfesenko10@gmail.com>
2025-05-11 15:41:41 -0400

    fixed platform, generational win for me

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,25 +7,24 @@ import {getCurrentUser} from "@/lib/actions/auth.action";
 import {getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/general.action"
 // import { redirect } from "next/navigation";
 
-const Page = async () => {
+async function Home() {
     const user = await getCurrentUser();
 
-    const [userInterviews, latestInterviews] = await Promise.all([
-        await getInterviewsByUserId(user?.id!),
-        await getLatestInterviews({ userId: user?.id! })
-    ])
+    const [userInterviews, allInterview] = await Promise.all([
+        getInterviewsByUserId(user?.id!),
+        getLatestInterviews({ userId: user?.id! }),
+    ]);
 
     const hasPastInterviews = userInterviews?.length! > 0;
-    const hasUpcomingInterviews = latestInterviews?.length! > 0;
+    const hasUpcomingInterviews = allInterview?.length! > 0;
 
     return (
         <>
-            <section className = "card-cta">
+            <section className="card-cta">
                 <div className="flex flex-col gap-6 max-w-lg">
                     <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-
                     <p className="text-lg">
-                        Practice on real interview questions & get instant feedback
+                        Practice real interview questions & get instant feedback
                     </p>
 
                     <Button asChild className="btn-primary max-sm:w-full">
@@ -33,35 +32,60 @@ const Page = async () => {
                     </Button>
                 </div>
 
-                <Image src="/robot.png" alt="robo-dude" width={300} height={300} className="max-sm:hidden"/>
+                <Image
+                    src="/robot.png"
+                    alt="robo-dude"
+                    width={400}
+                    height={400}
+                    className="max-sm:hidden"
+                />
             </section>
 
-            <section className = "flex flex-col gap-6 mt-8">
+            <section className="flex flex-col gap-6 mt-8">
                 <h2>Your Interviews</h2>
 
                 <div className="interviews-section">
                     {hasPastInterviews ? (
                         userInterviews?.map((interview) => (
-                            <InterviewCard{...interview} key={interview.id} />
-                        ))) :  (
+                            <InterviewCard
+                                key={interview.id}
+                                userId={user?.id}
+                                interviewId={interview.id}
+                                role={interview.role}
+                                type={interview.type}
+                                techstack={interview.techstack}
+                                createdAt={interview.createdAt}
+                            />
+                        ))
+                    ) : (
                         <p>You haven&apos;t taken any interviews yet</p>
                     )}
                 </div>
             </section>
 
-            <section className = "flex flex-col gap-6 mt-8">
-                <h2>Take an Interview</h2>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take Interviews</h2>
 
                 <div className="interviews-section">
                     {hasUpcomingInterviews ? (
-                        latestInterviews?.map((interview) => (
-                            <InterviewCard{...interview} key={interview.id} />
-                        ))) :  (
-                            <p>There are no new interviews available.</p>
+                        allInterview?.map((interview) => (
+                            <InterviewCard
+                                key={interview.id}
+                                userId={user?.id}
+                                interviewId={interview.id}
+                                role={interview.role}
+                                type={interview.type}
+                                techstack={interview.techstack}
+                                createdAt={interview.createdAt}
+                            />
+                        ))
+                    ) : (
+                        <p>There are no interviews available</p>
                     )}
                 </div>
             </section>
         </>
-    )
+    );
 }
-export default Page
+
+export default Home;
